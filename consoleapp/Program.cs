8c18cfb14c8f6282ceb2057e66cc6646dc9e1c71
Andres Mauricio Rodriguez Garzon <anmrodriguezga@unal.edu.co>
2025-06-06 13:13:15 -0500

    Clase 20: Implementacion de libreria Humanizer para numeros en palabras.

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -1,10 +1,13 @@
-﻿Console.WriteLine("Por favor ingrese su nombre:");
+﻿using System;
+using Humanizer;
+
+Console.WriteLine("Por favor ingrese su nombre:");
 string nombre = Console.ReadLine();
 
 Console.WriteLine("Por favor ingrese su cargo:");
 string cargo = Console.ReadLine();
 
 Console.WriteLine("Por favor ingrese su edad:");
-string edad = Console.ReadLine();
+int edad = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"Mi nombre es {nombre}, tengo {edad} años y soy {cargo}.");
+Console.WriteLine($"Mi nombre es {nombre}, tengo {edad.ToWords(new System.Globalization.CultureInfo("es"))} años y soy {cargo}.");
